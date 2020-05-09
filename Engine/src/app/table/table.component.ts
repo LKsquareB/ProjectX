301816c11ffa39d08342143a6087202a90ed2527
Lakshit Kohli <lakshitkohli5@gmail.com>
2020-05-09 11:12:03 +0530

    basic table code added

--- Engine/src/app/table/table.component.ts
+++ Engine/src/app/table/table.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  displayedColumns: string[] = ['',''];
+  // dataSource=Table_data;
   constructor() { }
 
   ngOnInit(): void {
