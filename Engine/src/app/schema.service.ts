299c0292cb4419bec91e66891febf6fa57bf7151
kanavbhatia <kanavbhatia97@gmail.com>
2020-05-09 15:51:06 +0530

    select implementation

--- Engine/src/app/schema.service.ts
+++ Engine/src/app/schema.service.ts
@@ -54,7 +54,11 @@ export class SchemaService {
                 color: 'primary',
                 isVisible: true,
                 isEnable: true,
-              }
+              },
+              elements: [
+                { name: 'Kanav' },
+                { name: 'Lakshit' },
+                { name: 'Ritika'}]
             },
             {
               type: 'radio',
@@ -88,32 +92,32 @@ export class SchemaService {
           ],
         },
       },
-      {
-        type: 'TABLE',
-        properties: {
-          uiProperty: {
-            width: '50%',
-            isVisible: true,
-          },
-          elements: [
-              {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-              {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-              {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-              {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-              {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-              {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-              {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-              {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-              {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-              {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-            ]
-          // elements:[
-          //   {name:'Kanav', sex: 'Male'},
-          //   {name:'Lakshit', age:21},
-          // ]
-        }
+      // {
+      //   type: 'TABLE',
+      //   properties: {
+      //     uiProperty: {
+      //       width: '50%',
+      //       isVisible: true,
+      //     },
+      //     elements: [
+      //       { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+      //       { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+      //       { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+      //       { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+      //       { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+      //       { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+      //       { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+      //       { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+      //       { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+      //       { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+      //     ]
+      //     // elements:[
+      //     //   {name:'Kanav', sex: 'Male'},
+      //     //   {name:'Lakshit', age:21},
+      //     // ]
+      //   }
 
-      }
+      // }
     ];
     return dataSource
   }
