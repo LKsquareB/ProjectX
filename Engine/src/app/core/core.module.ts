5aed14ed288d1d0ee7ac955bb68b4de1410ea696
kanavbhatia <kanavbhatia97@gmail.com>
2020-05-15 02:15:16 +0530

    snackbar foxes

--- Engine/src/app/core/core.module.ts
+++ Engine/src/app/core/core.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 import { DialogComponent } from './dialog/dialog.component';
 
-import {MatInputModule} from '@angular/material/input';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 
@@ -13,9 +10,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
   declarations: [SnackbarComponent, DialogComponent],
   imports: [
     CommonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
     MatSnackBarModule
   ],
   exports:[
